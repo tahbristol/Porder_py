4b2ded34ac38d8b9f17e6e65d4bd7028b4065cfd
tahbristol <tahbristol@gmail.com>
2018-05-19 15:33:24 -0400

    connected forms to signup/login

--- porder.py
+++ porder.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, url_for
+from forms import SignupForm, LoginForm
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'd65cee974fbbc376f5d5866c69dade58'
 
 @app.route('/')
 @app.route('/home')
@@ -12,11 +15,13 @@ def about():
 
 @app.route('/users/signup')
 def signup():
-	return render_template('users/new.html')
+	form = SignupForm()
+	return render_template('users/new.html', form=form)
 
 @app.route('/users/login')
 def login():
-	return render_template('users/login.html')
+	form = LoginForm()
+	return render_template('users/login.html', form=form)
 
 
 if __name__ == '__main__':
