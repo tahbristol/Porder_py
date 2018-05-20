b25302b4abba038e6432d88dbe226f397b6fa65b
tahbristol <tahbristol@gmail.com>
2018-05-19 23:44:43 -0400

    form errors

--- forms.py
+++ forms.py
@@ -3,15 +3,10 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class SignupForm(FlaskForm):
-	name = StringField('Name',
-	 						validators=[DataRequired(),
-						 	Length(min=2, max=20)])
-	email = StringField('Email',
-						validators=[DataRequired(), Email()])
-	password = PasswordField('Password',
-	 						validators=[DataRequired()])
-	password_confirmation = PasswordField('Confirm Password',
-										 validators=[DataRequired(), EqualTo('password')])
+	name = StringField('Name', validators=[DataRequired(), Length(min=2, max=20)])
+	email = StringField('Email', validators=[DataRequired(), Email()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	password_confirmation = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
 	submit = SubmitField('Sign Up')
 
 class LoginForm(FlaskForm):
